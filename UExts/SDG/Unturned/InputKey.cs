8188d74d191466d465acbe536d37877081b81ff0
Yurii <41921371+nn653@users.noreply.github.com>
2022-06-29 01:01:07 +0300

    feat(InputKey): add InputKey enum

---
+++ UExts/SDG/Unturned/InputKey.cs
@@ -0,0 +1,16 @@
+﻿public enum InputKey
+{
+    Jump = 0,
+    Primary = 1,
+    Secondary = 2,
+    Crouch = 3,
+    Prone = 4,
+    Sprint = 5,
+    LeanLeft = 6,
+    LeanRight = 7,
+    PluginKey1 = 9,
+    PluginKey2 = 10,
+    PluginKey3 = 11,
+    PluginKey4 = 12,
+    PluginKey5 = 13,
+}
