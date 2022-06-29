51eebc902419155d27dc8ab8571cf7eea0c0baba
EternalFG <72083320+EternalFG@users.noreply.github.com>
2022-06-29 20:44:32 +0200

    feat(PlayerInputEx): add class and IsKey method

--- UExts/SDG/Unturned/Players/PlayerInputEx.cs
+++ UExts/SDG/Unturned/Players/PlayerInputEx.cs
@@ -1,7 +1,12 @@
-﻿namespace UExts.SDG.Unturned.Players
+﻿using SDG.Unturned;
+
+namespace UExts.SDG.Unturned.Players
 {
     public static class PlayerInputEx
     {
-
+        public static bool PlayerInput(this PlayerInput player, InputKeys key)
+        {
+           return player.keys[(int)key];
+        }
     }
 }
