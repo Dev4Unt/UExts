a741ed50662e0b9d986057268f9b3f96d3636671
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-29 17:50:49 +0300

    feat(MagnitudeEx): add MagnitudeEx class

---
+++ UExts/Unity/MagnitudeEx.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UExts.Unity
+{
+    public static class MagnitudeEx
+    {
+        public static bool IsInRange(this Vector3 source, Vector3 target, float sqrValue)
+        {
+            return (source - target).sqrMagnitude <= sqrValue;
+        }
+    }
+}
