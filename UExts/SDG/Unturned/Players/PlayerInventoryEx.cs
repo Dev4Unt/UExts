90d2ce616ae2b367542213a5034ad39e256d4707
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-29 16:49:35 +0300

    feat(PlayerInventoryEx): add method Clear

--- UExts/SDG/Unturned/Players/PlayerInventoryEx.cs
+++ UExts/SDG/Unturned/Players/PlayerInventoryEx.cs
@@ -1,6 +1,40 @@
-﻿namespace UExts.SDG.Unturned.Players
+﻿using SDG.Unturned;
+using System;
+
+namespace UExts.SDG.Unturned.Players
 {
     public static class PlayerInventoryEx
     {
+        public static PlayerInventory Clear(this PlayerInventory source,
+            Predicate<ItemJar> onBeforeItemRemovedCallback = null,
+            Action<ItemJar> onItemRemovedCallback = null,
+            Action<ItemJar> onItemRemoveCanceled = null)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (byte p = 0; p < PlayerInventory.PAGES - 2; p += 1)
+            {
+                for (byte i = 0; i < source.items[p].items.Count; i++)
+                {
+                    ItemJar itemJar = source.items[p].getItem(i);
+
+                    bool? allow = onBeforeItemRemovedCallback?.Invoke(itemJar);
+                    if (allow.HasValue && allow.Value)
+                    {
+                        source.removeItem(p, i);
+                        onItemRemovedCallback?.Invoke(itemJar);
+                    }
+                    else
+                    {
+                        onItemRemoveCanceled?.Invoke(itemJar);
+                    }
+                }
+            }
+
+            return source;
+        }
     }
 }
