a4ce92eecd00e19b591dd1b0fefb9dc637cf66f2
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-29 15:09:46 +0300

    refactor(InputKeys): add namespace to enum

--- UExts/SDG/Unturned/Players/InputKeys.cs
+++ UExts/SDG/Unturned/Players/InputKeys.cs
@@ -1,16 +1,19 @@
-﻿public enum InputKeys
+﻿namespace UExts.SDG.Unturned.Players
 {
-    Jump = 0,
-    Primary = 1,
-    Secondary = 2,
-    Crouch = 3,
-    Prone = 4,
-    Sprint = 5,
-    LeanLeft = 6,
-    LeanRight = 7,
-    PluginKey1 = 9,
-    PluginKey2 = 10,
-    PluginKey3 = 11,
-    PluginKey4 = 12,
-    PluginKey5 = 13,
+    public enum InputKeys
+    {
+        Jump = 0,
+        Primary = 1,
+        Secondary = 2,
+        Crouch = 3,
+        Prone = 4,
+        Sprint = 5,
+        LeanLeft = 6,
+        LeanRight = 7,
+        PluginKey1 = 9,
+        PluginKey2 = 10,
+        PluginKey3 = 11,
+        PluginKey4 = 12,
+        PluginKey5 = 13,
+    }
 }
