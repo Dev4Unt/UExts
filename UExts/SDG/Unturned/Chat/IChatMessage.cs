2129c177263515223ba550c4782b38990903961d
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-30 16:42:13 +0300

    feat(IChatMessage): add IChatMessage interface

---
+++ UExts/SDG/Unturned/Chat/IChatMessage.cs
@@ -0,0 +1,23 @@
+﻿using SDG.Unturned;
+using System;
+using UnityEngine;
+
+namespace UExts.SDG.Unturned.Chat
+{
+    public interface IChatMessage
+    {
+        string Text { get; set; }
+
+        Color Color { get; set; }
+
+        SteamPlayer Sender { get; set; }
+
+        SteamPlayer Receiver { get; set; }
+
+        EChatMode Mode { get; set; }
+
+        Uri Icon { get; set; }
+
+        bool Rich { get; set;  }
+    }
+}
