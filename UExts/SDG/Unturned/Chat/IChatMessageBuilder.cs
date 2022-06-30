38db56b546c4b3a90e902a1fe233b2f641c39a1e
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-30 16:42:23 +0300

    feat(IChatMessageBuilder): add IChatMessageBuilder interface

---
+++ UExts/SDG/Unturned/Chat/IChatMessageBuilder.cs
@@ -0,0 +1,27 @@
+﻿using SDG.Unturned;
+using System;
+using UnityEngine;
+
+namespace UExts.SDG.Unturned.Chat
+{
+    public interface IChatMessageBuilder
+    {
+        IChatMessageBuilder SetText(string content);
+
+        IChatMessageBuilder SetReceiver(SteamPlayer player);
+
+        IChatMessageBuilder SetSender(SteamPlayer player);
+
+        IChatMessageBuilder SetColor(Color value);
+
+        IChatMessageBuilder SetMode(EChatMode value);
+
+        IChatMessageBuilder SetIcon(Uri uri);
+
+        IChatMessageBuilder SetIcon(string url);
+
+        IChatMessageBuilder UseRich();
+
+        IChatMessage Build();
+    }
+}
