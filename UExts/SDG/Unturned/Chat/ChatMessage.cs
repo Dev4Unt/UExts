55980a0cd748edc173aaddd6386e0220cf648d29
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-30 16:41:36 +0300

    feat(ChatMessage): add ChatMessage class

---
+++ UExts/SDG/Unturned/Chat/ChatMessage.cs
@@ -0,0 +1,23 @@
+﻿using SDG.Unturned;
+using System;
+using UnityEngine;
+
+namespace UExts.SDG.Unturned.Chat
+{
+    public class ChatMessage : IChatMessage
+    {
+        public string Text { get; set; }
+
+        public Color Color { get; set; }
+
+        public SteamPlayer Sender { get; set; }
+
+        public SteamPlayer Receiver { get; set; }
+
+        public EChatMode Mode { get; set; }
+
+        public Uri Icon { get; set; }
+
+        public bool Rich { get; set; }
+    }
+}
