5b77157be3cb74ea5fe4160f47b15b36e1cb8377
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-06-30 16:41:58 +0300

    feat(ChatMessageBuilder): add ChatMessageBuilder class

---
+++ UExts/SDG/Unturned/Chat/ChatMessageBuilder.cs
@@ -0,0 +1,77 @@
+﻿using SDG.Unturned;
+using System;
+using UnityEngine;
+
+namespace UExts.SDG.Unturned.Chat
+{
+    public class ChatMessageBuilder : IChatMessageBuilder
+    {
+        private readonly IChatMessage message;
+
+
+
+        public ChatMessageBuilder(IChatMessage message)
+        {
+            this.message = message;
+        }
+
+        public ChatMessageBuilder() : this(new ChatMessage())
+        {
+        }
+
+        
+
+        public IChatMessageBuilder SetColor(Color value)
+        {
+            message.Color = value;
+            return this;
+        }
+
+        public IChatMessageBuilder SetIcon(Uri uri)
+        {
+            message.Icon = uri;
+            return this;
+        }
+
+        public IChatMessageBuilder SetIcon(string url)
+        {
+            message.Icon = new Uri(url);
+            return this;
+        }
+
+        public IChatMessageBuilder SetMode(EChatMode value)
+        {
+            message.Mode = value;
+            return this;
+        }
+
+        public IChatMessageBuilder SetReceiver(SteamPlayer player)
+        {
+            message.Receiver = player;
+            return this;
+        }
+
+        public IChatMessageBuilder SetSender(SteamPlayer player)
+        {
+            message.Sender = player;
+            return this;
+        }
+
+        public IChatMessageBuilder SetText(string content)
+        {
+            message.Text = content;
+            return this;
+        }
+
+        public IChatMessageBuilder UseRich()
+        {
+            message.Rich = true;
+            return this;
+        }
+
+        public IChatMessage Build()
+        {
+            return message;
+        }
+    }
+}
