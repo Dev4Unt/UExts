87279f632a3d97d857777e24cf25ffc1410049e8
EternalFG <72083320+EternalFG@users.noreply.github.com>
2022-07-03 12:53:19 +0200

    feat(PlayerInputEx): add HasInputed, LastInputed method

--- UExts/SDG/Unturned/Players/PlayerInputEx.cs
+++ UExts/SDG/Unturned/Players/PlayerInputEx.cs
@@ -4,9 +4,19 @@ namespace UExts.SDG.Unturned.Players
 {
     public static class PlayerInputEx
     {
-        public static bool PlayerInput(this PlayerInput player, InputKeys key)
+        public static bool IsKey(this PlayerInput player, InputKeys key)
         {
            return player.keys[(int)key];
+        }
+
+        public static bool HasInputed(this PlayerInput player)
+        {
+            return Reflections.ReflectionFieldAccessor<PlayerInput>.GetValue<bool>("hasInputed", player);
+        }
+
+        public static float LastInuted(this PlayerInput player)
+        {
+            return Reflections.ReflectionFieldAccessor<PlayerInput>.GetValue<float>("lastInputed", player);
         }
     }
 }
