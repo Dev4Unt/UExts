55d70db587bd7cbb86608e9f79ea8e46068cd4fc
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-07-04 11:21:59 +0300

    refactor(PlayerInputEx): refactor naming and add new method IsDismissed and add enum InputKeys

--- UExts/SDG/Unturned/Players/PlayerInputEx.cs
+++ UExts/SDG/Unturned/Players/PlayerInputEx.cs
@@ -1,22 +1,45 @@
 ﻿using SDG.Unturned;
+using UExts.Reflections;
 
 namespace UExts.SDG.Unturned.Players
 {
+    public enum InputKeys
+    {
+        Jump = 0,
+        Primary = 1,
+        Secondary = 2,
+        Crouch = 3,
+        Prone = 4,
+        Sprint = 5,
+        LeanLeft = 6,
+        LeanRight = 7,
+        PluginKey1 = 9,
+        PluginKey2 = 10,
+        PluginKey3 = 11,
+        PluginKey4 = 12,
+        PluginKey5 = 13,
+    }
+
     public static class PlayerInputEx
     {
-        public static bool IsKey(this PlayerInput player, InputKeys key)
+        public static bool IsKey(this PlayerInput source, InputKeys key)
         {
-           return player.keys[(int)key];
+           return source.keys[(int)key];
         }
 
-        public static bool HasInputed(this PlayerInput player)
+        public static bool HasInputed(this PlayerInput source)
         {
-            return Reflections.ReflectionFieldAccessor<PlayerInput>.GetValue<bool>("hasInputed", player);
+            return ReflectionFieldAccessor<PlayerInput>.GetValue<bool>("hasInputed", source);
         }
 
-        public static float LastInuted(this PlayerInput player)
+        public static float LastInputed(this PlayerInput source)
         {
-            return Reflections.ReflectionFieldAccessor<PlayerInput>.GetValue<float>("lastInputed", player);
+            return ReflectionFieldAccessor<PlayerInput>.GetValue<float>("lastInputed", source);
+        }
+
+        public static bool IsDismissed(this PlayerInput source)
+        {
+            return ReflectionFieldAccessor<PlayerInput>.GetValue<bool>("isDismissed", source);
         }
     }
 }
