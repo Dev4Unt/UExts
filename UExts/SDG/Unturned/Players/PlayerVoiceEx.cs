ca1efee8e61aae7f754d5ba7984f77418854ac7b
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-07-04 11:22:58 +0300

    feat(PlayerVoiceEx): add PlayerVoiceEx class and methods
    InputWantsToRecord,
    AvaliablePlayBackTime

---
+++ UExts/SDG/Unturned/Players/PlayerVoiceEx.cs
@@ -0,0 +1,18 @@
+﻿using SDG.Unturned;
+using UExts.Reflections;
+
+namespace UExts.SDG.Unturned.Players
+{
+    public static class PlayerVoiceEx
+    {
+        public static bool InputWantsToRecord(this PlayerVoice source)
+        {
+            return ReflectionFieldAccessor<PlayerVoice>.GetValue<bool>("inputWantsToRecord", source);
+        }
+
+        public static float AvaliablePlayBackTime(this PlayerVoice source)
+        {
+            return ReflectionFieldAccessor<PlayerVoice>.GetValue<float>("availablePlaybackTime", source);
+        }
+    }
+}
