797e891c76e4147ce2f1250be86f6fbacfb3cbb9
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-07-04 11:19:35 +0300

    feat(LightingEx): LightingEx add class and enum TimesOfDay
    LastWindTime,
    IsCycled,
    WindDelay.
    GetTimeOfDay

---
+++ UExts/SDG/Unturned/Lighting/LightingEx.cs
@@ -0,0 +1,51 @@
+﻿using SDG.Unturned;
+using System;
+using UExts.Reflections;
+
+namespace UExts.SDG.Unturned.Lighting
+{
+    public enum TimesOfDay
+    {
+        Night,
+        FullMoon,
+        Day,
+    }
+
+    public static class LightingEx
+    {
+        public static float LastWindTime()
+        {
+            return ReflectionFieldAccessor<LightingManager>.GetValue<float>("lastWind");
+        }
+
+        public static bool IsCycled()
+        {
+            return ReflectionFieldAccessor<LightingManager>.GetValue<bool>("isCycled");
+        }
+
+        public static float WindDelay()
+        {
+            return ReflectionFieldAccessor<LightingManager>.GetValue<float>("windDelay");
+        }
+
+        public static TimesOfDay GetTimeOfDay()
+        {
+            if (LightingManager.isDaytime)
+            {
+                return TimesOfDay.Day;
+            }
+
+            if (LightingManager.isNighttime)
+            {
+                return TimesOfDay.Night;
+            }
+
+            if (LightingManager.isFullMoon)
+            {
+                return TimesOfDay.FullMoon;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
