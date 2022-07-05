fd7d4eec61da0b21753cb2bc87a9171e58d7114e
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-07-05 12:32:15 +0300

    refactor(ChatCommunicationEx): remove SayToAll method

--- UExts/SDG/Unturned/Chat/ChatCommunicationEx.cs
+++ UExts/SDG/Unturned/Chat/ChatCommunicationEx.cs
@@ -4,11 +4,6 @@ namespace UExts.SDG.Unturned.Chat
 {
     public static class ChatCommunicationEx
     {
-        public static void SayToAll(this IChatMessage source)
-        {
-            ChatManager.say(source.Text, source.Color, source.Rich);
-        }
-
         public static void SendMessage(this IChatMessage source)
         {
             ChatManager.serverSendMessage(source.Text, source.Color, source.Sender, source.Receiver, source.Mode, source.Icon.AbsoluteUri, source.Rich);
