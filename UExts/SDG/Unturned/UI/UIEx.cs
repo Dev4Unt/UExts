c72df0c4e724b1165cb5006af3b37e296c9f9fe8
sunnamed434 <65300126+sunnamed434@users.noreply.github.com>
2022-07-25 12:55:38 +0300

    feat(UIEx): UIEx add class also add 2 methods DisableEverythingInUI and EnableEverythingInUI

---
+++ UExts/SDG/Unturned/UI/UIEx.cs
@@ -0,0 +1,41 @@
+﻿using SDG.Unturned;
+
+namespace UExts.SDG.Unturned.UI
+{
+    public static class UIEx
+    {
+        public static Player DisableEverythingInUI(this Player source)
+        {
+            source.disablePluginWidgetFlag(EPluginWidgetFlags.Modal
+                | EPluginWidgetFlags.ShowHealth
+                | EPluginWidgetFlags.ShowFood
+                | EPluginWidgetFlags.ShowWater
+                | EPluginWidgetFlags.ShowVirus
+                | EPluginWidgetFlags.ShowStamina
+                | EPluginWidgetFlags.ShowLifeMeters
+                | EPluginWidgetFlags.ShowStatusIcons
+                | EPluginWidgetFlags.ShowDeathMenu
+                | EPluginWidgetFlags.ShowInteractWithEnemy
+                | EPluginWidgetFlags.ShowUseableGunStatus
+                | EPluginWidgetFlags.ShowVehicleStatus);
+            return source;
+        }
+
+        public static Player EnableEverythingInUI(this Player source)
+        {
+            source.enablePluginWidgetFlag(EPluginWidgetFlags.Modal
+                | EPluginWidgetFlags.ShowHealth
+                | EPluginWidgetFlags.ShowFood
+                | EPluginWidgetFlags.ShowWater
+                | EPluginWidgetFlags.ShowVirus
+                | EPluginWidgetFlags.ShowStamina
+                | EPluginWidgetFlags.ShowLifeMeters
+                | EPluginWidgetFlags.ShowStatusIcons
+                | EPluginWidgetFlags.ShowDeathMenu
+                | EPluginWidgetFlags.ShowInteractWithEnemy
+                | EPluginWidgetFlags.ShowUseableGunStatus
+                | EPluginWidgetFlags.ShowVehicleStatus);
+            return source;
+        }
+    }
+}
